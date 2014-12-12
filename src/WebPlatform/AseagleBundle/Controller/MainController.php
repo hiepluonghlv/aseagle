47f8ea443d51a1b46819d00a15021bfb75af6a0b
hiepluonghlv <hiepluonghlv@gmail.com>
2014-12-13 00:32:08 +0800

    Revert "login, register integration"
    This reverts commit abd5cd6d337f192d5af524789906cd6b97ea65e1.

--- src/WebPlatform/AseagleBundle/Controller/MainController.php
+++ src/WebPlatform/AseagleBundle/Controller/MainController.php
@@ -221,11 +221,4 @@ class MainController extends Controller
         return new Response(json_encode($mapped_products_info),200,array('Content-Type'=>'application/json'));
     }
 
-    public function getTokenAction()
-    {
-        return new Response($this->container->get('form.csrf_provider')
-            ->generateCsrfToken('authenticate'));
-    }
-
-
 }
